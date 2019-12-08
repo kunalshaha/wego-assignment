9a73d495fdf57dc07e1104816273f660435676f6
kunalshaha <kunalsshaha@gmail.com>
2019-12-08 21:19:29 +0800

    latest changes

--- parking-management/src/main/java/com/wego/parkingmanagement/exception/CustomExceptionHandler.java
+++ parking-management/src/main/java/com/wego/parkingmanagement/exception/CustomExceptionHandler.java
@@ -1,17 +1,21 @@
 package com.wego.parkingmanagement.exception;
 
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
 @ControllerAdvice
 @RestController
-public class CustomExceptionHandler {
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ParkingExceptionHandler.class)
     public final ResponseEntity<ErrorDetails> handleInterNalErrors(ParkingExceptionHandler ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
@@ -27,13 +31,20 @@ public class CustomExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
-   /* @Override
-    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleTypeMismatch(ex, headers, status, request);
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        System.out.println(ex.getParameterName());
+        String msg = ex.getParameterName() + " is mandatory parameter ";
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), msg,
+                request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
     @Override
-    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleMissingPathVariable(ex, headers, status, request);
-    }*/
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String msg = "Latitude/Longitude must be numerical value";
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), msg,
+                request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 }
